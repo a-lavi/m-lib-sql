3a72f1c28f579bdf4b4562b6384e386519bc2e30
a-lavi <ali.rahmani.63@icloud.com>
2022-08-31 19:52:01 +0200

    add files

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+
+const app = express()
+
+app.set('view engine', 'ejs')
+const books =[
+    {
+        author : 'Mark Twain',
+        title: 'Hukleberry Finn',
+        releaseYear : 1884
+    },
+    {
+        author : 'Frank Herbert',
+        title: 'Dune',
+        releaseYear : 1965
+    }
+]
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res)=>{
+res.render('greeting')
+})
+
+app.get('/api/book', (req , res)=>{
+    res.json(books)
+})
+app.listen(port)
